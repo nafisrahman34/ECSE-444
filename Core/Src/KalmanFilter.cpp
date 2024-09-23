bdd4771ed2b8f6219755fe63bb67306f6729a313
Rong Wei Zhang <rong.w.zhang@mail.mcgill.ca>
2024-09-23 19:44:52 -0400

    Initial commit

---
+++ Core/Src/KalmanFilter.cpp
@@ -0,0 +1,18 @@
+/*
+ * KalmanFilter.cpp
+ *
+ *  Created on: Sep 23, 2024
+ *      Author: rongweizhang
+ */
+
+#include "KalmanFilter.h"
+
+KalmanFilter::KalmanFilter() {
+	// TODO Auto-generated constructor stub
+
+}
+
+KalmanFilter::~KalmanFilter() {
+	// TODO Auto-generated destructor stub
+}
+
